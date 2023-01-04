34add3dac9086d29960f3c06947e8c05b115a218
akasharc <121471129+akasharc@users.noreply.github.com>
2023-01-04 19:44:09 +0530

    Add files via upload

---
+++ Exception1/src/com/akash/one/Test2.java
@@ -0,0 +1,26 @@
+package com.akash.one;
+
+public class Test2 {
+	public static void main(String[] args) {
+
+		System.out.println("First line");
+		System.out.println("Second line");
+		System.out.println("Third line");
+
+		try {
+			int a = 10 / 0;
+		
+		System.out.println("Extra line");
+		System.out.println("Extra1 line");
+		}
+		
+		catch (Exception e) {
+			System.out.println("velocity");
+		}
+
+	
+	}
+
+}
+
+
